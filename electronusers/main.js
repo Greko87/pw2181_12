7480533e817950185d0cf189f4601de122a1b2bd
Greko87 <33939100+Greko87@users.noreply.github.com>
2018-05-04 12:26:45 -0600

    Electron Users

---
+++ electronusers/main.js
@@ -0,0 +1,24 @@
+//Aplicacion general 
+const app=require('electron').app;
+//Uso de las pantallas del sistema
+const BrowserWindow=require('electron').BrowserWindow;
+//Ruta de la carpeta base
+const path=require('path');
+//URL de las páginas
+const url=require('url');
+//Standart de ECMASCRIPT 6 - JS
+let PantallaPrincipal;
+
+function muestraPantallaPrincipal(){
+	PantallaPrincipal=new BrowserWindow({width:620,height:825});
+	PantallaPrincipal.loadURL(url.format({
+		//join: concatenar cadenas
+		pathname:path.join(__dirname,'index.html'),
+		protocol: 'file',
+		slashed: true;
+	}));
+	//clic derecho, inspeccionar en chrome
+	PantallaPrincipal.webContents.openDevTools();
+	PantallaPrincipal.show();
+}
+app.on('ready',muestraPantallaPrincipal);
