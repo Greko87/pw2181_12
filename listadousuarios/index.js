32f3c39a2f04e3767096e60d7d65c170d34ffb62
Greko87 <33939100+Greko87@users.noreply.github.com>
2018-05-11 13:00:20 -0600

    Listado de Usuarios - Pasar a DetalleUsuario

--- listadousuarios/index.js
+++ listadousuarios/index.js
@@ -6,11 +6,23 @@ const path = require('path')
 const url = require('url')
 
 let pantallaDetalle;
+var usuarios = new Array(20);
+
+function datos(nombre,genero,foto,direccion,telefono){
+	this.nombre = nombre;
+	this.genero = genero;
+	this.foto = foto;
+	this.direccion = direccion;
+	this.telefono = telefono;
+}
 
 function inicia(){
 	var resultado = "";
 	var nombre = "";
 	var foto = "";
+	var genero = "";
+	var direccion = "";
+	var telefono = "";
 	$.ajax({
  		url: 'https://randomuser.me/api/?results=20',
  		dataType: 'json',
@@ -18,11 +30,35 @@ function inicia(){
  			for(var i=0;i<20;i++){
  				nombre=data.results[i].name.first+" "+data.results[i].name.last;
  				foto=data.results[i].picture.medium;
+ 				genero=data.results[i].gender;
+ 				direccion=data.results[i].location.street;
+ 				telefono=data.results[i].phone;
  				resultado="<li><img src="+foto+">"+nombre+"<button id="+i+">Detalle</button>";
  				$("#lstUsuarios").append(resultado);
+ 			usuarios[i] = new datos(nombre,genero,foto,direccion,telefono);
+
  			}
  		}
 	});
 }
+
+function botonDetalle(){
+	//alert(this.id);
+	//alert(usuarios[this.id].nombre);
+	require('electron').remote.getGlobal('infoUsuarios').nombre=usuarios[this.id].nombre; //para que se vaya al main.js
+	require('electron').remote.getGlobal('infoUsuarios').genero=usuarios[this.id].genero;
+	require('electron').remote.getGlobal('infoUsuarios').foto=usuarios[this.id].foto;
+	require('electron').remote.getGlobal('infoUsuarios').direccion=usuarios[this.id].direccion;
+	require('electron').remote.getGlobal('infoUsuarios').telefono=usuarios[this.id].telefono;
+	pantallaDetalle = new BrowserWindow({width:320,height:425});
+	pantallaDetalle.loadURL(url.format({
+		pathame: path.join(__dirname,'detalleUsuarios.html'),//pasar los datos
+		protocol: 'file',
+		slashes: true
+	}));
+	//pantallaDetalle.webContents.openDevTools();
+	pantallaDetalle.show();
+}
+$("body").on("click","li >button,botonDetalle);
 
 inicia();
